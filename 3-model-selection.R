31edbeaaeb9719c35214cd79fd36916016fe94c3
pennydy <dpan@vassar.edu>
2019-09-23 18:32:01 -0400

    Test parameters for random.walk.model

--- 3-model-selection.R
+++ 3-model-selection.R
@@ -14,10 +14,22 @@
 
 # You don't need to get a perfect match. Just get in the ballpark. 
 
+random.walk.test <- random.walk.model(1000,drift=0.015, sdrw=0.3, criterion=5)
+sum(random.walk.test$correct) / length(random.walk.test$correct)
+random.walk.correct.data <- random.walk.test %>% filter(correct==TRUE)
+mean(random.walk.correct.data$rt)
+random.walk.incorrect.data <- random.walk.test %>% filter(correct==FALSE)
+mean(random.walk.incorrect.data$rt)
+
+
+accumulator.model <- function(samples, rate.1=40, rate.2=40, criterion=3)
+accumulator.test <- accumulator.model(1000)
+mean(accumulator.test$rt)
 
 # Can both models do a reasonable job of accounting for the mean RT and accuracy? Report the
 # results of your efforts:
 
+# random.walk.model: drift=0.015, sdrw=0.3, criterion=5
 
 # Using the parameters that you found above, plot histograms of the distribution of RTs
 # predicted by each model. Based on these distributions, what kind of information could
